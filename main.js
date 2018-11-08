55a3b2acb1ad41fa8ef627b3f1e4ee6c06e0c234
MamoruDS <jlchen2015@gmail.com>
2018-11-08 18:32:14 +0800

    added function confFormatCheck
    fixed bug in getts 'Date()'->'Date.now()'

--- main.js
+++ main.js
@@ -28,7 +28,9 @@ function writeConf(data) {
         logGen('default conf.json has been ganerated.', 'info')
     }
     data = JSON.stringify(data)
-    data = beautify(data, {indent_size: 4})
+    data = beautify(data, {
+        indent_size: 4
+    })
     fs.writeFileSync(conf_path, data, 'utf8')
 }
 
@@ -36,7 +38,7 @@ function createUserInfo(user, overwrite) {
     let _conf = readConf()
     let default_userInfo = {
         type_range: ["8", "16", "512"],
-        update_ts: getTimestamp(),
+        update_ts: getTimestamp(true),
         cookie: "undefined"
     }
     if (!_conf.user_info[user] || overwrite) {
@@ -328,7 +330,7 @@ function dynamicInfoParser(d_info) {
 }
 
 let getTimestamp = (timeout_bool = false) => {
-    let cur_ts = Math.round(Date() / 1000)
+    let cur_ts = Math.round(Date.now() / 1000)
     if (timeout_bool) {
         return cur_ts - timeout
     } else {
@@ -407,7 +409,52 @@ function updateCheck(last_update_id) {
         })
 }
 
+let confFormatCheck = () => {
+    let _conf = undefined
+    if (fs.existsSync(conf_path)) {
+        _conf = readConf()
+        // writeConf(conf)
+    } else {
+        writeConf()
+    }
+
+    let user_info = _conf.user_info
+    if (user_info) {
+        for (user in user_info) {
+            let user_detail = user_info[user]
+            let user_re = {
+                cookie: user_detail.cookie,
+                cookie_valid: user_detail.cookie_valid,
+                type_range: user_detail.type_range,
+                update_ts: user_detail.update_ts
+            }
+            if (user_detail.notify !== undefined) {
+                if (!user_detail.type_range) {
+                    user_re.type_range = user_detail.notify
+                }
+            }
+            if (user_detail.notify_ts !== undefined) {
+                if (!user_detail.update_ts) {
+                    user_re.update_ts = Math.max(...user_detail.notify_ts)
+                }
+            }
+            if (user_detail.cookie_valid === undefined) {
+                user_re.cookie_valid = false
+            }
+            if (!user_detail.type_range) {
+                user_re.type_range = ["8", "16", "512"]
+            }
+            if (!user_detail.update_ts) {
+                user_re.update_ts = getTimestamp(true)
+            }
+            _conf.user_info[user] = user_re
+        }
+    }
+    writeConf(_conf)
+}
+
 logGen('bot started. interval(sec):' + interval_sec.toString().bold.green + ' timeout:' + timeout.toString().bold.red, 'info')
+confFormatCheck()
 
 let task = schedule.scheduleJob(`*/${interval_sec} * * * * *`, function () {
     let _conf = readConf()
